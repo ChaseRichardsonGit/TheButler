3a5f072f4fc1b2082d2fca15fd105d64088f7646
Chase Richardson <chaserichardson+git@gmail.com>
2023-01-27 14:46:02 -0500

    Added weather function

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require('dotenv').config(); 
+const weather = require('./src/weather.js');
+const calculateCost = require('./src/calccost.js');
+const OWMapiKey = process.env.OWMapiKey;
+
+const fs = require('fs');
 
 const Discord = require('discord.js');
 
@@ -34,9 +39,20 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     });
 
+client.on('messageCreate', async function(message){
+  if(message.channel.type === Discord.ChannelType.DM) {
+    let now = new Date();
+    fs.appendFileSync('logs/openai.log', `[${now.toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' })}] ${message.author.username}: ${message.content}\n`);
+    }
+    });
+
 client.on("messageCreate", async function(message){
   if(message.author.bot) return;
-    
+  if (message.content.startsWith('/weather')) {
+    const zip = message.content.split(' ')[1];
+    if(!zip) return message.channel.send("Please provide a zip code after the command")
+    weather.getWeather(zip, message, OWMapiKey);
+} else   
   if(message.channel.type === Discord.ChannelType.DM) {
     console.log("Received a direct message from " + message.author.username + ": " + message.content);   
     try {
@@ -52,14 +68,9 @@ client.on("messageCreate", async function(message){
         stop: ""
       });
 
-      function calculateCost(total_tokens) {
-        let cost_per_1000_tokens = 0.02;
-        return (total_tokens/1000) * cost_per_1000_tokens;
-      }
-
       let response = gptResponse.data.choices[0].text.trim(); 
       let total_tokens = (gptResponse.data.usage.total_tokens);
-      let cost = calculateCost(total_tokens);
+      let cost = calculateCost.calculateCost(total_tokens);
       let costTrimmed = parseFloat(cost.toFixed(4));
       console.log(`\x1b[33mToken:${total_tokens}\x1b[0m,\x1b[32mTransCost:${costTrimmed}\x1b[0m`)
       if(response.length > 1999){
