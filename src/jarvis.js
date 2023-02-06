1d224240175d133dba9291700e6536e89ae3cf23
Chase Richardson <chaserichardson+git@gmail.com>
2023-02-06 12:47:24 -0500

    Files

---
+++ src/jarvis.js
@@ -0,0 +1,97 @@
+// Load the environment variables
+require('dotenv').config(); 
+
+// Load the external functions
+const openai = require('./openai.js');
+
+// Get your persona from your environment
+let whoami = process.env.WHOAMI;
+
+// Load the Discord 
+const Discord = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+// Define Mongo and load the database
+const { UserInfo, Link, Log } = require('./mongo.js'); 
+
+// Define Intents and Partials for Discord
+const client = new Client({ 
+	intents: [ 
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+	GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+	],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.User,
+    ]
+}); 
+
+// Listener for General only console logs for right now. 
+client.on('messageCreate', async function(message){
+    if(message.channel.type !== Discord.ChannelType.DM) {
+    if(message.author.bot) return; {
+    if(message.content.includes('!puerus'));
+        console.log('Message received from ' + message.author.username + ': ' + message.content);
+    }}
+});
+
+// Listener for Direct Message OpenAI Dialogue
+client.on('messageCreate', async function(message){
+    if(message.channel.type === Discord.ChannelType.DM) {
+    if(message.author.bot) return; 
+       openai.callopenai(message);
+    }
+});
+
+// Listener to Log Direct Messages
+client.on('messageCreate', async function(message){
+    if(message.channel.type === Discord.ChannelType.DM) {
+      if(message.author.bot) return;
+        const log = new Log({
+        bot: whoami,
+        server: "-",
+        channel: "directMessage",
+        username: message.author.username,
+        message: message.content,
+        time: new Date().toString()
+      });
+    
+      log.save().then(() => {
+        console.log(`Message logged to MongoDB: ${message.author.username}: ${message.content}\n`);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+});
+
+// Listener to Log Bot Direct Message Responses
+client.on('messageCreate', async function(message){
+    if(message.channel.type === Discord.ChannelType.DM) {
+    if(message.author.bot) {
+    const log = new Log({
+        bot: whoami,
+        server: "-",
+        channel: "directMessage",
+        username: message.author.username,
+        message: message.content,
+        time: new Date().toString(),
+    });
+    
+    log.save().then(() => {
+        console.log(`Message logged to MongoDB: ${message.author.username}: ${message.content}\n`);
+    }).catch(err => {
+        console.error(err);
+    })
+    }}
+});
+
+console.log(`${whoami} is online!\n`);
+
+module.exports = client;
