4d0b1341921f00db5e5949d582c3938659d9b771
Chase Richardson <chaserichardson+git@gmail.com>
2023-02-13 11:44:19 -0500

    Somewhat working site

--- public/app.js
+++ public/app.js
@@ -1,23 +1,38 @@
 const chatWindow = document.querySelector('#chat-window');
 const userInput = document.querySelector('#user-input');
 const sendButton = document.querySelector('#send-button');
+const usernameInput = document.querySelector('#username-input');
+const usernameSubmitButton = document.querySelector('#username-submit-button');
+let username = '';
 
-sendButton.addEventListener('click', async () => { 
-  console.log('Button clicked!');
-  const message = userInput.value;
-  console.log('Input value:', message);
-  userInput.value = '';
-  addMessage('user', message);
-  const response = await getResponse(message);
-  console.log('Bot Response:', response);
-  addMessage('bot', response);
+usernameSubmitButton.addEventListener('click', () => {
+  username = usernameInput.value.trim();
+  if (username) {
+    usernameInput.value = '';
+    usernameInput.disabled = true;
+    usernameSubmitButton.disabled = true;
+    console.log('Username set:', username);
+  }
 });
 
-async function getResponse(prompt) {
-  const url = `/api/response?prompt=${encodeURIComponent(prompt)}`;
-  const response = await fetch(url);
-  return response.text();
-}
+sendButton.addEventListener('click', async () => {
+  try {
+    console.log('Button clicked!');
+    const message = userInput.value;
+    console.log('Input value:', message);
+    userInput.value = '';
+    addMessage(username, message); // Pass the username variable to addMessage
+    if (typeof openai.generateResponse !== 'function') {
+      throw new Error('The generateResponse function is not defined');
+    }
+    const response = await openai.generateResponse(message);
+    console.log('Bot Response:', response);
+    addMessage('bot', response);
+  } catch (error) {
+    console.error(error);
+    addMessage('bot', 'Sorry, an error occurred. Please try again.');
+  }
+});
 
 function addMessage(sender, message) {
   const div = document.createElement('div');
